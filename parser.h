98376781ecc8188099579f81e524256ae7756d5b
zard <aparecidohallyson@gmail.com>
2025-03-09 10:37:36 -0300

    metodos definidos

--- parser.h
+++ parser.h
@@ -3,9 +3,21 @@
 
 #ifndef PARSER_H
 #define PARSER_H
+#include "token.h"
 
+typedef struct Parser
+{
+    const char *src;
+    Token current_token;
+};
 
-
+void parse_program(Parser * parser);
+void parse_function_header(Parser * parser);
+void parse_block(Parser * parser);
+void parse_statement(Parser* parser);
+void parse_variable_declaration(Parser * parser);
+void advance(Parser *parser);
+void eat(Parser *parser, TokenType expected);
 
 
 #endif
